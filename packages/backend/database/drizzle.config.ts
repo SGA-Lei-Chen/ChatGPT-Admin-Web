57991f53199ad13a68a48df89a2c0ed8dcf7e0cd
AprilNEA <github@sku.moe>
2025-07-13 11:30:01 +0800

    feat: add local sync db powerd by electricql

--- packages/backend/database/drizzle.config.ts
+++ packages/backend/database/drizzle.config.ts
@@ -1,21 +1,23 @@
 import "dotenv/config";
 
-import { defineConfig } from "drizzle-kit";
+import { type Config, defineConfig } from "drizzle-kit";
 
-const dbUrl = process.env.DATABASE_URL;
+export const dbUrl = process.env.DATABASE_URL;
 
 if (!dbUrl) {
   throw "DATABASE_URL is not set";
 }
 
-export default defineConfig({
+export const drizzleConfig: Config = {
   dialect: "postgresql",
   schema: "./src/schema/**/*.ts",
-  out: "./migrations/",
+  out: "./drizzle/migrations/",
   dbCredentials: {
     url: dbUrl,
   },
   verbose: process.env.NODE_ENV === "development",
   breakpoints: false,
   strict: true,
-});
+};
+
+export default defineConfig(drizzleConfig);
