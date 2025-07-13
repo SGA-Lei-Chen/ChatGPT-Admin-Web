9fddb90814bf95e752807127acc3761aaf85fadd
AprilNEA <github@sku.moe>
2025-07-13 12:54:42 +0800

    refactor: improve database package init

--- packages/backend/database/src/index.ts
+++ packages/backend/database/src/index.ts
@@ -1,2 +1,17 @@
-export * from "./schema";
-export * from "./utils";
+import { drizzle } from "drizzle-orm/node-postgres";
+import * as schema from "./schema";
+import pg from "pg";
+
+export const dbPool = (databaseUrl: string) =>
+  new pg.Pool({
+    connectionString: databaseUrl,
+  });
+
+export const getDb = (pool: pg.Pool) =>
+  drizzle(pool, {
+    schema: {
+      ...schema,
+    },
+  });
+
+export type DataBase = ReturnType<typeof drizzle<typeof schema>>;
--- packages/backend/server/src/lib/database/index.ts
+++ packages/backend/server/src/lib/database/index.ts
@@ -1,25 +1,11 @@
-import * as schema from "@achat/database/schema";
-import { drizzle } from "drizzle-orm/node-postgres";
-import pg from "pg";
+import { dbPool, getDb } from "@achat/database";
 
-const getDbPool = () => {
-  return new pg.Pool({
-    connectionString: process.env.DATABASE_URL,
-  });
-};
-
-const globalForDB = globalThis as unknown as {
-  dbPool: ReturnType<typeof getDbPool> | undefined;
-};
-
-const dbPool = globalForDB?.dbPool ?? getDbPool();
-
-const db = drizzle(dbPool, { schema });
-
-export type DataBase = ReturnType<typeof drizzle<typeof schema>>;
-
-if (process.env.NODE_ENV !== "production") {
-  globalForDB.dbPool = dbPool;
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set");
 }
 
+const pool = dbPool(process.env.DATABASE_URL);
+const db = getDb(pool);
+
 export default db;
+export type DataBase = typeof db;
